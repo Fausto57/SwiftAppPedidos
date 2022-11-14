a28daf496c8cfc890ea532f24c9139aa0533afa2
Fausto57 <56176187+Fausto57@users.noreply.github.com>
2022-11-14 09:36:33 -0700

    Comienzo de la aplicacion

---
+++ PedidosExamen/PedidosExamen/PedidosExamenApp.swift
@@ -0,0 +1,20 @@
+//
+//  PedidosExamenApp.swift
+//  PedidosExamen
+//
+//  Created by CCDM03 on 14/11/22.
+//
+
+import SwiftUI
+
+@main
+struct PedidosExamenApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
